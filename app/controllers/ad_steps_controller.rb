25ec0ae35072da34ae7154dd42b9c9c19b72a3b0
MamoonaT <mamoonatahir17@gmail.com>
2022-09-12 16:46:26 +0500

    created various forms for posting an add

--- app/controllers/ad_steps_controller.rb
+++ app/controllers/ad_steps_controller.rb
@@ -1,9 +1,48 @@
 class AdStepsController < ApplicationController
-	include Wicked::Wizard
-	steps :picture, :contact
+  include Wicked::Wizard
+  steps :details, :picture, :contact
+  before_action :find_ad, only: [:show, :update]
 
-	def show
-		@ad = Ad.new
-		render_wizard 	
-	end
+  def show
+    render_wizard
+  end  
+  
+  def update
+    case step
+    when :picture
+      @ad.update(detail_params)
+    when :contact
+      @ad.update(image_params)
+    end
+    if params[:id] == 'wicked_finish'
+      @ad.update(contact_params)
+      render "wicked_finish", notice: "Thanks for posting an AD."
+    else
+      render_wizard
+    end
+  end
+
+  def wicked_finish; end
+
+  private
+  
+  def find_ad
+    @ad = Ad.find_by(id: params[:ad].present? ? params[:ad][:ad_id] : params[:ad_id])
+    @ad = Ad.new if @ad.blank?
+  end
+
+  def detail_params
+    params.require(:ad).permit(:city, :color, :car_make, :engine_type, :milage, :price, :transmission_type, :engine_capacity, :add_discription, :assembly_type, :primary_contact, :secondary_contact)
+  end
+
+  def image_params
+    params.require(:ad).permit(pictures: [])
+  end
+
+  def contact_params
+    permitted_params = params.require(:ad).permit(:primary_contact, :secondary_contact)
+    permitted_params.merge!(user_id: current_user.id)
+  end
+
 end
+
--- app/models/ad.rb
+++ app/models/ad.rb
@@ -1,3 +1,11 @@
 class Ad < ApplicationRecord
+	has_many_attached :images
+	validate :count
 	# belongs_to :user
+
+	private
+	def count
+		return if images.count <= 2
+		errors.add(:images, 'You can upload max 2 images')
+	end
 end
