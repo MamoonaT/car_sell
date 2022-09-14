046f2a86defcc7ab1d38feee3ddc7b2e10fd84d2
MamoonaT <mamoonatahir17@gmail.com>
2022-09-14 16:50:10 +0500

    added a progress bar for the form

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -8,6 +8,6 @@ class Ad < ApplicationRecord
 
 CITY = [['Lahore','lahore'],['Karachi' , 'karachi'],['Islamabad' , 'islamabad'],['Quetta' , 'quetta'],['Peshawar', 'peshawar'],['Rawalpindi', 'rawalpindi']]
 COLOR = [['White' , 'white'],['Blue' , 'blue'],['Black', 'black'],['Grey', 'grey'],['Silver' , 'silver']]	
-
+CAR_MAKE = [['Honda', 'honda'],['Suzuki', 'suzuki'],['Toyota', 'toyata'],['Tesla', 'tesla']]
 end
 
